41697106f7f1d36207d8f2935f646d993ccf08f0
Emmanuele Carlone <emmanuele.carlone@runelab.it>
2025-05-04 12:12:54 +0200

    Commentata gestione cors

--- src/application.ts
+++ src/application.ts
@@ -26,6 +26,7 @@ export class BackendApplication extends BootMixin(
     dotenv.config();
 
     // Configure CORS
+    /*
     this.options.rest = {
       ...this.options.rest,
       cors: {
@@ -42,6 +43,7 @@ export class BackendApplication extends BootMixin(
         'trust proxy': false,
       },
     };
+    */
 
     // Configure Cognito
     this.bind('cognito.region').to(process.env.COGNITO_REGION || '');
